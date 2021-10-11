42a5b348afff4f5d85fb17a5d27a5f88108fc7d5
paulwhite93 <91712154+paulwhite93@users.noreply.github.com>
2021-10-11 14:26:36 -0700

    Update processes.cpp

--- processes.cpp
+++ processes.cpp
@@ -3,7 +3,6 @@
 #include <unistd.h>      // for fork, pipe, dup, close
 #include <stdio.h>       // for NULL, perror
 #include <stdlib.h>      // for exit
-
 #include <iostream>      // for cout
 
 using namespace std;
@@ -15,6 +14,12 @@ int main( int argc, char** argv ) {
         cerr << "Usage: processes command" << endl;
         exit( -1 );
     }
+    if(pipe(fds[0])<0){                             // create a pipe using fds[0]
+        perror("pipe error");
+    }            
+    if(pipe(fds[1])<0){                             // create a pipe using fds[1]
+        perror("pipe2 error");
+    }
     if ( ( pid = fork( ) ) < 0 ) {                      // fork a child
         perror( "fork error" );
     }
@@ -22,6 +27,9 @@ int main( int argc, char** argv ) {
         int pid2;
         if(pipe(fds[0])<0){                             // create a pipe using fds[0]
             perror("pipe error");
+        }            
+        if(pipe(fds[1])<0){                             // create a pipe using fds[1]
+            perror("pipe2 error");
         }
 
         if((pid2=fork())<0){                            // fork a grand-child
@@ -29,43 +37,37 @@ int main( int argc, char** argv ) {
         }
         else if (pid == 0){                             // if I'm a grand-child
                                                         //I'm a grand-child    
-            if(pipe(fds[1])<0){                         // create a pipe using fds[1]
-                perror("pipe2 error");
-            }
+
 
             int pid3;
             if((pid3 = fork())<0){                      // fork a great-grand-child
                 perror("Fork error on great-grand-child");
             }
             else if (pid == 0){                         // if I'm a great-grand-child  
-                dup2(fds[1][1],1);
+                dup2(fds[0][1],1);
                 close(fds[1][0]);
                 close(fds[0][1]);
-                execlp("ps","-A",NULL);           // execute "ps"
+                execlp("ps","ps","-A",NULL);           // execute "ps"
             }
             else                                       // else if I'm a grand-child
-                dup2(fds[1][0],0);
+                dup2(fds[0][0],0);
                 close(fds[1][1]);
                 close(fds[0][0]);
-                dup2(fds[0][1],1);            
-                execlp("grep",argv[1],NULL);    // execute "grep"
+                dup2(fds[1][1],1);            
+                execlp("grep","grep",argv[1],NULL);    // execute "grep"
             }
         else {
                                                         // else if I'm a child
             close(fds[0][1]);
             close(fds[1][0]);
             close(fds[1][1]);
-            dup2(fds[0][0],0);                              //reads from fds[0]
-            execlp("wc","-l",NULL);               // execute "wc"
+            dup2(fds[1][0],0);                              //reads from fds[0]
+            execlp("wc","wc","-l",NULL);               // execute "wc"
         }
         
     } 
     else {                                              // I'm a parent
         //make sure pipes are closed
-        close(fds[0][1]);
-        close(fds[0][0]);
-        close(fds[1][0]);
-        close(fds[1][1]);
         wait( NULL );
         cout << "commands completed" << endl;
     }
