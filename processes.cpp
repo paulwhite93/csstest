c2531756d6db4ab3202ade1ea31af2b6c7e1989b
paulwhite93 <91712154+paulwhite93@users.noreply.github.com>
2021-10-11 13:04:52 -0700

    Update processes.cpp

--- processes.cpp
+++ processes.cpp
@@ -20,40 +20,40 @@ int main( int argc, char** argv ) {
     }
     else if ( pid == 0 ) {                              // I'm a child 
         int pid2;
-        if(pipe(fds[0]))<0{                             // create a pipe using fds[0]
+        if(pipe(fds[0])<0{                              // create a pipe using fds[0]
             perror("pipe error");
         }
-        dup2(fds[0][read],read);                        //reads from fds[0]
-        close(fds[0][write]);
+        dup2(fds[0][0],read);                        //reads from fds[0]
+        close(fds[0][1]);
         if((pid2=fork())<0){                            // fork a grand-child
-          perror("Fork error on grand-child")
+          perror("Fork error on grand-child");
         }
         else if (pid == 0){                             // if I'm a grand-child
                                                         //I'm a grand-child    
             if(pipe(fds[1])<0){                         // create a pipe using fds[1]
                 perror("pipe2 error");
             }
-            dup2(fds[1][read],read);
-            close(fds[1][write]);
-            close(fds[0][read]);
-            dup2(fds[0][write],write);
+            dup2(fds[1][0],read);
+            close(fds[1][1]);
+            close(fds[0][0]);
+            dup2(fds[0][1],write);
             int pid3;
             if((pid3 = fork())<0){                        // fork a great-grand-child
-                perror("Fork error on great-grand-child")
+                perror("Fork error on great-grand-child");
             }
             else if (pid == 0){                         // if I'm a great-grand-child  
-                dup2(fds[1][write],write);
-                close(fds[1][read]);
-                close(fds[0][write];
-                execlp("ps -A");                        // execute "ps"
+                dup2(fds[1][1],write);
+                close(fds[1][0]);
+                close(fds[0][1];
+                execlp("ps","ps","-A",NULL);                        // execute "ps"
             }
             else                                            // else if I'm a grand-child
-                execlp("grep argv[1]");                     // execute "grep"
+                execlp("grep","grep",argv[1],NULL);                     // execute "grep"
             }
         else {
                                                         // else if I'm a child
 
-            execlp("wc -l");                              // execute "wc"
+            execlp("wc","wc","-l",NULL);                              // execute "wc"
         }
     } 
     else {                                              // I'm a parent
